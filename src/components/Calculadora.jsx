fa46c66293a444e563fe369e3d3f680bab8e4dfa
Cristian <cristianeduardovillegasmarin@gmail.com>
2022-12-09 07:44:06 -0500

    firts commit

---
+++ src/components/Calculadora.jsx
@@ -0,0 +1,89 @@
+import React from "react"
+import { useState } from "react";
+import "./Calculator.css"
+
+import { Container } from '@mui/material';
+import { Box } from "@mui/system";
+
+export const Calculadora = () => {
+
+  const [num,setNum]=useState(0)
+  const [oldNum,setOldNum]=useState(0)
+  const [operator,setOperator]=useState()
+
+function inputNum(e){
+  let input=e.target.value
+if(num===0){
+  setNum(input)
+}else{
+  setNum(num+input)
+}
+}
+
+function clear(){
+  setNum(0)
+}
+
+function porcentage(){
+  setNum(num/100)
+}
+
+function changeSing(){
+  if(num>0){
+    setNum(-num)
+  }else{
+    setNum(Math.abs(num))
+  }
+}
+function operatorHandler(e){
+let operatorInput= e.target.value
+setOperator(operatorInput)
+setOldNum(num)
+setNum(0)
+}
+
+function calculate(){
+  if(operator==="/"){
+    setNum(parseFloat(oldNum) / parseFloat(num))
+  }else if(operator === "X"){
+    setNum(parseFloat(oldNum) * parseFloat(num))
+  }else if(operator=== "-"){
+    setNum(parseFloat(oldNum) - parseFloat(num))
+  }else if(operator==="+"){
+    setNum( parseFloat(oldNum) + parseFloat(num))
+  }
+
+}
+
+  return (
+    <div>
+    <Box m={5}/>
+    <Container maxWidth="xs">
+ <div className="wrapper">
+ <Box m={12}/>
+  <h1 className="result">{num}</h1>
+ <button className="pink" onClick={clear}>AC</button>
+ <button className="pink" onClick={changeSing}>+/-</button>
+ <button className="pink" onClick={porcentage}>%</button>
+ <button className="orange" onClick={operatorHandler} value="/">/</button>
+ <button className="gray" onClick={inputNum} value={7}>7</button>
+ <button className="gray"  onClick={inputNum} value={8}>8</button>
+ <button className="gray"  onClick={inputNum} value={9}>9</button>
+ <button className="orange" onClick={operatorHandler} value="X">X</button>
+ <button className="gray"  onClick={inputNum} value={4}>4</button>
+ <button className="gray"  onClick={inputNum} value={5}>5</button>
+ <button className="gray" onClick={inputNum} value={6}>6</button>
+ <button className="orange" onClick={operatorHandler} value="-">-</button>
+ <button className="gray"  onClick={inputNum} value={1}>1</button>
+ <button className="gray"  onClick={inputNum} value={2}>2</button>
+ <button className="gray"  onClick={inputNum} value={3}>3</button>
+ <button className="orange" onClick={operatorHandler} value="+">+</button>
+ <button className="gray"  onClick={inputNum} value={0}>0</button>
+ <button className="gray" onClick={inputNum} value={"."}>,</button>
+ <button className="gray" style={{visibility:"hidden"}}>,</button>
+ <button className="orange" onClick={calculate}>=</button>
+ </div>
+ </Container>
+ </div>
+  )
+}
